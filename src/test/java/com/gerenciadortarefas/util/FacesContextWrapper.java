49ed722663a2040c14e9203475da74fd595e5130
allefe1 <allefefilipe1@gmail.com>
2025-05-22 18:24:05 -0300

    correção de bugs

--- src/test/java/com/gerenciadortarefas/util/FacesContextWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.gerenciadortarefas.util;
-
-import javax.faces.context.FacesContext;
-
-public class FacesContextWrapper {
-    public FacesContext getCurrentInstance() {
-        return FacesContext.getCurrentInstance();
-    }
-}
